8dba345a155b6641953bda66bcbe8ad4f6401be3
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-09-18 21:09:18 +0530

    Main Panel Prepared...

--- app/src/main/java/rakesh/app/bustimingapp/MainActivity.java
+++ app/src/main/java/rakesh/app/bustimingapp/MainActivity.java
@@ -3,6 +3,8 @@ package rakesh.app.bustimingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Clicked(View view){
+        Toast.makeText(this,"clicked",Toast.LENGTH_SHORT).show();
     }
 }
