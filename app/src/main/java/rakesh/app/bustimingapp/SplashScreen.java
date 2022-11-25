e18858023540942a84cd0f37350ae1b9d333996e
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-11-26 01:02:46 +0530

    splash screen is working...

--- app/src/main/java/rakesh/app/bustimingapp/SplashScreen.java
+++ app/src/main/java/rakesh/app/bustimingapp/SplashScreen.java
@@ -2,7 +2,11 @@ package rakesh.app.bustimingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import rakesh.app.bustimingapp.Home.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Hander and delayed to show the splash screen for second then open the home screen
+        // show the logo then start the next activity after the 2sec
+        // when you back the logo will not show.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        },2000);
     }
 }
