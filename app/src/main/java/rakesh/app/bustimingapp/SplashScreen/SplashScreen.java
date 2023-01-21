085332b96d16300618818de4a1cdba4739972be8
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2023-01-21 12:27:32 +0530

    just checking for searching function but it's paid now I will think for the alternative.

--- app/src/main/java/rakesh/app/bustimingapp/SplashScreen/SplashScreen.java
+++ app/src/main/java/rakesh/app/bustimingapp/SplashScreen/SplashScreen.java
@@ -23,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), SearchingAbilityCheck.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
         },1000);
